3f1300a9f6332ddc60613ee4c466876cc4f3fe85
Egor Poderyagin <egor@crazyrussian.pro>
2017-04-12 11:18:59 +0300

    added simple api for CRUD TodoItems

---
+++ backend/todo_item/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TodoItemConfig(AppConfig):
+    name = 'todo_item'
--- backend/todo_list/settings.py
+++ backend/todo_list/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'todo_item',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_list',
+        'USER': 'todo_list',
+        'PASSWORD': 'todo_list',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
