7c8bb6b2e67d8f5eb7d073e787e3de8d26b84faf
Egor Poderyagin <egor@crazyrussian.pro>
2017-04-12 15:32:53 +0300

    add create and delete functionality to TodoService

--- frontend/src/app/mock-todo-items.ts
+++
@@ -1,7 +0,0 @@
-import {TodoItem} from './todo-item';
-
-export const TODO_ITEMS: TodoItem[] = [
-  { id: 1, title: 'Awake', status: true},
-  { id: 2, title: 'Brush teeth', status: true},
-  { id: 3, title: 'Procrastinate', status: false},
-];
--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
-import { TODO_ITEMS } from './mock-todo-items';
 import { TodoItem } from './todo-item';
 
 @Injectable()
 export class TodoService {
+  private todoItemsUrl = 'api/web/todo_item/';
+
+  constructor(private http: Http) { }
+
   getTodoItems(): Promise<TodoItem[]> {
-    return Promise.resolve(TODO_ITEMS);
+    return this.http.get(this.todoItemsUrl)
+      .toPromise()
+      .then(response => response.json() as TodoItem[])
+      .catch(this.handleError);
+  }
+
+  createTodoItem(title: string): Promise<TodoItem> {
+    return this.http
+      .post(this.todoItemsUrl, {title: title})
+      .toPromise()
+      .then(res => res.json() as TodoItem)
+      .catch(this.handleError);
+  }
+
+  deleteTodoItem(id: number): Promise<void> {
+    const url = `${this.todoItemsUrl}${id}/`;
+    return this.http.delete(url)
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error accured', error);
+    return Promise.reject(error.message || error);
   }
 }
