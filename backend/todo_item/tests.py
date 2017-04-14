1db469d4b5628ccd81f934a2db8363384bb3c3b4
Egor Poderyagin <egor@crazyrussian.pro>
2017-04-14 13:52:08 +0300

    added websockets to backend

--- backend/todo_item/tests.py
+++ backend/todo_item/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from channels.test import ChannelTestCase, HttpClient
+from channels.signals import consumer_finished
 
-# Create your tests here.
+from .models import TodoItem
+
+
+class WebSocketTests(ChannelTestCase):
+    def test_creating_new_todo_items_send_notification(self):
+        client = HttpClient()
+        client.join_group("todo_list")
+
+        TodoItem.objects.create(title="test_item")
+
+        # ToDo: To know why next statement is necessary
+        # https://github.com/django/channels/issues/607
+        # consumer_finished.send(sender=None)
+
+        received = client.receive()
+
+        self.assertNotEquals(received, None)
