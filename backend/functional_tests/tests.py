386252ba840103254c9c8cf4dc0febc9a9db2a82
Egor Poderyagin <egor@crazyrussian.pro>
2017-04-15 11:58:32 +0300

    added functional tests

--- backend/functional_tests/tests.py
+++ backend/functional_tests/tests.py
@@ -25,11 +25,32 @@ class FunctionalTest(StaticLiveServerTestCase):
         input_box = self.browser.find_element_by_id('new-item-title')
         create_button = self.browser.find_element_by_id('new-item-create')
 
-        input_box.send_keys("Brush teeth")
+        input_box.send_keys(todo_title)
         create_button.click()
 
         # test new todo_must be created
         self.assertIn(
             todo_title,
             self.browser.find_element_by_tag_name("body").text
+        )
+
+    def test_can_see_todo_item_created_in_different_window_of_browser(self):
+        todo_title = "Brush teeth"
+
+        # connect to server from two browser instances
+        self.browser.get(self.live_server_url)
+        browser2 = webdriver.Chrome()
+        browser2.get(self.live_server_url)
+
+        # add new todo_item in first browser instance
+        input_box = self.browser.find_element_by_id('new-item-title')
+        create_button = self.browser.find_element_by_id('new-item-create')
+
+        input_box.send_keys(todo_title)
+        create_button.click()
+
+        # try to look at that text in second browser instance
+        self.assertIn(
+            todo_title,
+            browser2.find_element_by_tag_name("body").text
         )
