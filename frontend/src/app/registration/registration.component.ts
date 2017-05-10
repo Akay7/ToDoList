4f56f1789cfee670ff70487f4745aa085341736d
Egor Poderyagin <egor@crazyrussian.pro>
2017-05-10 20:32:56 +0300

    remove useless component

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { User } from '../user';
-import {isSuccess} from "@angular/http/src/http_utils";
 
 @Component({
   selector: 'app-registration',
